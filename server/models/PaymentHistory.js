6885ece0e5e6587f8c3cca6108042fc8caa78d3f
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-08-11 13:00:42 +0530

    timestamp added

--- server/models/PaymentHistory.js
+++ server/models/PaymentHistory.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
-const ads = new mongoose.Schema({
-  userID: { type: String, required: true },
-  BussinessID: { type: String, required: true },
-  txID: { type: String, required: true },
-});
+const ads = new mongoose.Schema(
+  {
+    userID: { type: String, required: true },
+    BussinessID: { type: String, required: true },
+    txID: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const Ads = mongoose.model("Ads", ads);
 module.exports = Ads;
